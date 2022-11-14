a5c1b3256e9abb36e05448f3d0d1449bd80fad25
lmy <1429418347@qq.com>
2022-11-14 16:56:41 +0800

    '头部底部完成'

--- js/rem.js
+++ js/rem.js
@@ -1,17 +1,20 @@
-
-(function () {
-    var supportsOrientationChange = 'onorientationchange' in window ? 'orientationchange' : 'resize';
-    var timeoutId;
-    function setRem() {
-        var clientWidth = document.documentElement.clientWidth;
-        var nowPX = clientWidth / 320 * 100;
-        document.documentElement.style.fontSize = nowPX + 'px';
+function setRem() {
+    var ui_w = 375;
+    var client_w = document.documentElement.clientWidth || document.body.clientWidth;
+    client_w = client_w > 800 ? 800 : client_w;
+    client_w = client_w < 350 ? 350 : client_w;
+    var html = document.getElementsByTagName('html')[0];
+    html.style.fontSize = (client_w / ui_w) * 10 + 'px';
+}
+setRem();
+var flg = true;
+window.onresize = function () {
+    if (flg) {
+        setRem();
+        flg = false;
     }
-    setRem();
-    window.addEventListener(supportsOrientationChange, function () {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(function () {
-            setRem();
-        }, 300);
-    }, false);
-})();
+    setTimeout(function () {
+        flg = true;
+    }, 200)
+
+}
