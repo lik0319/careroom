90caaa2595993d42a1829a17815908a62e8feeab
lmy <1429418347@qq.com>
2022-11-17 09:23:34 +0800

    '二手房列表js完成'

--- js/SecondhandList.js
+++ js/SecondhandList.js
@@ -5,34 +5,54 @@ var li_ = document.getElementsByClassName('li');
 var item_ = document.getElementsByClassName('item');
 var item_a_ = document.getElementsByClassName('item_a');
 var item_s_ = document.getElementsByClassName('item_s');
-console.log(item_s_);
-console.log(item_);
+// 点击导航栏变颜色 箭头变颜色 变上箭头
+var index_;
 for (var i = 0; i < li_.length; i++) {
     li_[i].setAttribute('index', i);
     li_[i].onclick = function () {
-        var index_ = this.getAttribute('index');
-        console.log(index_);
+        window.event ? window.event.cancelBubble = true : event.stopPropagation();
+        index_ = this.getAttribute('index');
         list.className = 'list ul';
         for (var j = 0; j < item_.length; j++) {
             item_[j].style.display = 'none';
             item_[index_].style.display = 'block';
         }
-        for (var a = 0; a < item_s_.length; j++) {
-            item_s_[a].style.display = '5px solid #ccc';
-            item_s_[index_].style.display = '5px solid #ff8a00';
-        }
         for (var k = 0; k < item_a_.length; k++) {
             item_a_[k].className = 'item_a';
             item_a_[index_].className = 'item_a red';
         }
-        // for (var d = 0; d < item_s_.length; d++) {
-        //     //  border-top: 5px solid #ccc;
-        //     item_s_[d].style.borderTop = '5px solid #ccc'
-        //     item_s_[index_].style.borderTop = '5px solid #ff8a00;'
-        //     console.log(item_s_[index_]);
-        // }
+        for (var s = 0; s < item_s_.length; s++) {
+            item_s_[s].style.borderTop = ' 5px solid #ccc'
+            item_s_[s].style.borderBottom = ' 5px solid transparent'
+            item_s_[s].style.top = '2rem';
+            item_s_[index_].style.borderTop = ' 5px solid transparent'
+            item_s_[index_].style.borderBottom = ' 5px solid #ff8a00'
+            item_s_[index_].style.top = '1.2rem';
+        }
+    }
+}
+// 点击页面导航内容恢复
+document.onclick = function () {
+    list.className = 'list';
+    for (var j = 0; j < item_.length; j++) {
+        item_[j].style.display = 'none';
+    }
+    for (var k = 0; k < item_a_.length; k++) {
+        item_a_[k].className = 'item_a';
+
+    }
+    for (var s = 0; s < item_s_.length; s++) {
+        item_s_[s].style.borderTop = ' 5px solid #ccc'
+        item_s_[s].style.borderBottom = ' 5px solid transparent'
+        item_s_[s].style.top = '2rem';
     }
 }
 
+for (var j = 0; j < item_.length; j++) {
+    item_[j].onclick = function () {
+        this.style.display = 'block';
+        window.event ? window.event.cancelBubble = true : event.stopPropagation();
+    }
+}
 
 
